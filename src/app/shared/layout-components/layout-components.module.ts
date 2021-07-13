1a96cb40a5a68bb0ee2018ba24fbf8f7b8d00efc
brandon.mendoza <brandon.mendoza@ayi.group>
2021-07-13 17:11:54 -0300

    ajustes

--- src/app/shared/layout-components/layout-components.module.ts
+++ src/app/shared/layout-components/layout-components.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { HeaderBaseComponent } from '../layout-components/main-header/header-base/header-base.component';
-import { HeaderDeskComponent } from '../layout-components/main-header/header-desk/header-desk.component';
-import { HeaderMobileComponent } from '../layout-components/main-header/header-mobile/header-mobile.component';
-import { FooterDeskComponent } from '../layout-components/footer/footer-desk/footer-desk.component';
-import { FooterMobileComponent } from '../layout-components/footer/footer-mobile/footer-mobile.component';
-import { FooterBaseComponent } from '../layout-components/footer/footer-base/footer-base.component';
+import { HeaderBaseComponent } from './main-header/header-base/header-base.component';
+import { HeaderDeskComponent } from './main-header/header-desk/header-desk.component';
+import { HeaderMobileComponent } from './main-header/header-mobile/header-mobile.component';
+import { FooterDeskComponent } from './footer/footer-desk/footer-desk.component';
+import { FooterMobileComponent } from './footer/footer-mobile/footer-mobile.component';
+import { FooterBaseComponent } from './footer/footer-base/footer-base.component';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
 import { MatIconModule } from '@angular/material/icon';
 import{MatToolbarModule} from'@angular/material/toolbar'
