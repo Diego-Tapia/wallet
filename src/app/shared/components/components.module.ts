d7390cf18f8dfee0c961ea11a0b7a040bc245553
diego.tapia <diego.tapia@ayi.group>
2021-07-14 10:41:52 -0300

    add component card and folders

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeDeskComponent } from './home/home-desk/home-desk.component';
 import { HomeMobileComponent } from './home/home-mobile/home-mobile.component';
+import { CardComponent } from './card/card.component';
+import {MatCardModule} from '@angular/material/card';
 
 
 
 @NgModule({
   declarations: [
     HomeDeskComponent,
-    HomeMobileComponent
+    HomeMobileComponent,
+    CardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule
   ]
 })
 export class ComponentsModule { }
