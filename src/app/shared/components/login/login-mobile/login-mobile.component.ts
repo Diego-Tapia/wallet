f5b704b1ae533d795aa6474dac675ef9843f6fce
brandon.mendoza <brandon.mendoza@ayi.group>
2021-08-11 19:41:06 -0300

    login en mobile y arreglo de login en desk

--- src/app/shared/components/login/login-mobile/login-mobile.component.ts
+++ src/app/shared/components/login/login-mobile/login-mobile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 @Component({
   selector: 'app-login-mobile',
   templateUrl: './login-mobile.component.html',
@@ -8,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class LoginMobileComponent implements OnInit {
 
   constructor() { }
-
+  myForm = new FormGroup({
+    userName: new FormControl('', Validators.required),
+    Password: new FormControl('', Validators.required),
+});
+  login() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.myForm.value);
+  }
   ngOnInit(): void {
   }
 
--- src/app/shared/components/login/login.module.ts
+++ src/app/shared/components/login/login.module.ts
@@ -4,10 +4,14 @@ import { RouterModule } from '@angular/router';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatTableModule } from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list'
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { LoginDeskComponent } from './login-desk/login-desk.component';
 import { LoginMobileComponent } from './login-mobile/login-mobile.component';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [LoginDeskComponent, LoginMobileComponent],
@@ -18,7 +22,12 @@ import { LoginMobileComponent } from './login-mobile/login-mobile.component';
     MatFormFieldModule,
     MatIconModule,
     MatCheckboxModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTableModule,
+    MatInputModule,
+    MatListModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class LoginModule { }
