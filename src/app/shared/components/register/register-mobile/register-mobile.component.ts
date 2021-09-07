7b0fd10184b6e6a2adc409683f19b59bcdc67041
brandon.mendoza <brandon.mendoza@ayi.group>
2021-09-07 13:10:54 -0300

    register-mobile new designe

--- src/app/shared/components/register/register-mobile/register-mobile.component.ts
+++ src/app/shared/components/register/register-mobile/register-mobile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormBuilder} from '@angular/forms';
 import { RegisterI } from '../models/register.interface';
 import { ApiRegisterService } from '../services/api-register.service';
 
@@ -9,19 +9,33 @@ import { ApiRegisterService } from '../services/api-register.service';
   styleUrls: ['./register-mobile.component.sass']
 })
 export class RegisterMobileComponent implements OnInit {
+  firstFormGroup!: FormGroup;
+  secondFormGroup!: FormGroup;
+  isEditable = false;
 
-  constructor(private apiregister: ApiRegisterService) { }
-  myForm = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.maxLength(16)]),
-    password: new FormControl('', [Validators.required, Validators.maxLength(16), Validators.minLength(8)]),
-    email: new FormControl ('', [Validators.required, Validators.email])
-});
+  constructor(private apiregister: ApiRegisterService,
+              private _formBuilder: FormBuilder) { }
+  
   ngOnInit(): void {
+    this.firstFormGroup = this._formBuilder.group({
+      name: new FormControl('', [Validators.required, Validators.maxLength(16)]),
+      lastname: new FormControl('', [Validators.required, Validators.maxLength(16)]),
+      dni: new FormControl('', [Validators.required, Validators.maxLength(16)]),
+      cuil: new FormControl('', [Validators.required, Validators.maxLength(16)]),
+      dateOfBirth: new FormControl('', [Validators.required, Validators.maxLength(16)]),
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.maxLength(16)]),
+      telefono: new FormControl('', [Validators.required, Validators.maxLength(16)]),
+      username: new FormControl('', [Validators.required, Validators.maxLength(16)]),
+      password: new FormControl('', [Validators.required, Validators.maxLength(16)]),
+      confirmPassword: new FormControl('', [Validators.required, Validators.maxLength(16)]),
+    });
   }
-  register(form: RegisterI) {
+  /*register(form: RegisterI) {
     this.apiregister.onRegister(form).subscribe(data => {
       console.log(data);
     });
     console.log(form)
-  }
+  }*/
 }
--- src/app/shared/routing/app-routing.module.ts
+++ src/app/shared/routing/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainLayoutComponent } from '../layout-components/main-layout/main-layout.component';
 import { HomeDeskComponent } from '../components/home/home-desk/home-desk.component';
-import { CreditDeskComponent } from '../components/credits/credit-desk/credit-desk.component';
 import { LoginDeskComponent } from '../components/login/login-desk/login-desk.component';
 import { RegisterDeskComponent } from '../components/register/register-desk/register-desk.component';
 import { ConfirmCodeDeskComponent } from '../components/confirm-code/confirm-code-desk/confirm-code-desk.component';
